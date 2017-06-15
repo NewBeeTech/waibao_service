0c0117d56a141b47b3c41ec8d0ce31ef86415f8a
huliyou <liyoupie@gmail.com>
2017-06-15 19:28:21 +0800

    更改更新方法

--- app/router.js
+++ app/router.js
@@ -2,6 +2,6 @@
 
 module.exports = app => {
   app.get('/api/jinqiangui/qq', 'jinqiangui.index');
-  app.put('/api/jinqiangui/qq/:qq', 'jinqiangui.update');
+  app.post('/api/jinqiangui/qq/:qq', 'jinqiangui.update');
   app.get('/api/jinqiangui/pay-info', 'jinqiangui.payInfo');
 };
